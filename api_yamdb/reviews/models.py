9a70c0400f756ef14d6c13e93d4b55b3115fd334
Imbafar <alfir.kalimullin@yandex.ru>
2021-11-30 00:09:39 +0500

    Test commit.Added some models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.TextField()
+    year = models.SmallIntegerField()
+    description = models.TextField(blank=True)
+    genre = models.ForeignKey(
+        Genres,
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+    )
+
+
+    def __str__(self):
+        return self.name
