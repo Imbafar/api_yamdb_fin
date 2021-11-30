de478ce1fd43bbb0de75c225537360da95b51bc6
Maxim <maxim94.sam@gmail.com>
2021-11-30 15:39:26 +0400

    Add Review&Comments models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,2 +1,36 @@
 from django.db import models
 
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.ForeignKey(
+        Titles, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
+        auto_now_add=True, db_index=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_review'
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.text[:15]
