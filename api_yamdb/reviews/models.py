caeeacc7087e6dfae9b25d353a0a42bda948f3ec
Alfir Kalimullin <alfir.kalimullin@yandex.ru>
2021-12-01 12:41:09 +0500

    12-40 01.12.2021 Added django-filters, search, fix models, serializers, fix requirements

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 
-# class Custom_User(AbstractUser):
-#     is_moderator = models.BooleanField(
-#         default=False,
-#     )
+class Custom_User(AbstractUser):
+    is_moderator = models.BooleanField(
+        default=False,
+    )
 
 
 User = get_user_model()
