255d5520308d0edc7bca917adcc625ef9045b516
Никита Чалых <Wizardus@list.ru>
2021-12-10 12:24:01 +0300

    fix import

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Category, Comment, Genre, Review, Title, User
-
 from .utils import CurrentTitleDefault
 
 
